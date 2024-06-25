77e4094d1f2db9afaaa6bac5be70ff16413c6e37
eric-yates <eric@lumenalabs.com>
2024-06-24 22:53:16 -0600

    minor: static creation method with name

--- src/Persona.ts
+++ src/Persona.ts
@@ -1,1 +1,17 @@
-export default class Persona {}
+export default class PersonaImpl implements Persona {
+    public static Class?: new () => Persona
+
+    public name?: string
+
+    protected constructor(name?: string) {
+        this.name = name
+    }
+
+    public static Create(name?: string): Persona {
+        return new (this.Class ?? this)(name)
+    }
+}
+
+export interface Persona {
+    name?: string
+}
--- src/__tests__/behavioral/Persona.test.ts
+++ src/__tests__/behavioral/Persona.test.ts
@@ -1,10 +1,25 @@
-import AbstractSpruceTest, { test, assert } from '@sprucelabs/test-utils'
-import Persona from '../../Persona'
+import AbstractSpruceTest, {
+    test,
+    assert,
+    generateId,
+} from '@sprucelabs/test-utils'
+import PersonaImpl from '../../Persona'
 
 export default class PersonaTest extends AbstractSpruceTest {
     @test()
     protected static async canCreatePersona() {
-        const persona = new Persona()
+        const persona = this.Persona()
         assert.isTruthy(persona)
+    }
+
+    @test()
+    protected static async personaAcceptsName() {
+        const name = generateId()
+        const persona = this.Persona(name)
+        assert.isEqual(persona.name, name)
+    }
+
+    private static Persona(name?: string) {
+        return PersonaImpl.Create(name)
     }
 }
