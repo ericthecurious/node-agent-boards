0cb747e54bed17eda8deebaa1e3579737f99fc2d
eric-yates <eric@lumenalabs.com>
2024-07-04 03:57:57 -0600

    major: add personas to board meeting

--- src/BoardMeeting.ts
+++ src/BoardMeeting.ts
@@ -1,11 +1,19 @@
+import { assertOptions } from '@sprucelabs/schema'
+import Persona from './Persona'
+
 export default class BoardMeetingImpl implements BoardMeeting {
     public static Class?: new () => BoardMeeting
 
     protected constructor() {}
 
-    public static Create() {
+    public static Create(options: BoardMeetingOptions) {
+        assertOptions(options, ['personas'])
         return new (this.Class ?? this)()
     }
 }
 
 export interface BoardMeeting {}
+
+export interface BoardMeetingOptions {
+    personas: Persona[]
+}
--- src/__tests__/behavioral/BoardMeeting.test.ts
+++ src/__tests__/behavioral/BoardMeeting.test.ts
@@ -1,10 +1,27 @@
-import AbstractSpruceTest, { test, assert } from '@sprucelabs/test-utils'
+import AbstractSpruceTest, {
+    test,
+    assert,
+    errorAssert,
+} from '@sprucelabs/test-utils'
 import BoardMeetingImpl from '../../BoardMeeting'
 
 export default class BoardMeetingTest extends AbstractSpruceTest {
     @test()
     protected static async canCreateBoardMeeting() {
-        const instance = BoardMeetingImpl.Create()
+        const instance = this.BoardMeeting()
         assert.isTruthy(instance)
+    }
+
+    @test()
+    protected static async throwsWithMissingRequiredOptions() {
+        // @ts-ignore
+        const err = assert.doesThrow(() => BoardMeetingImpl.Create({}))
+        errorAssert.assertError(err, 'MISSING_PARAMETERS', {
+            parameters: ['personas'],
+        })
+    }
+
+    private static BoardMeeting() {
+        return BoardMeetingImpl.Create({ personas: [] })
     }
 }
